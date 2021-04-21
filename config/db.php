cfe8e0c5051c0a6589c1dad374e5cc4cf1da3397
mettaworld00 <mettaworldfit@gmail.com>
2021-04-21 13:29:04 -0400

    www

--- config/db.php
+++ config/db.php
@@ -5,7 +5,13 @@ class Database
 
 	public static function connect()
 	{
-		$db = new mysqli('localhost', 'root', '', 'dbsystem');
+
+		$SERVER_NAME = "localhost";
+		$USER_NAME = "root";
+		$PASSWORD = "";
+		$DATABASE_NAME = $_SESSION['DATABASE_NAME'];
+
+		$db = new mysqli($SERVER_NAME,$USER_NAME, $PASSWORD, $DATABASE_NAME);
 		$db->query("SET NAMES 'utf8'");
 
 		if ($db->connect_errno) {
@@ -19,9 +25,13 @@ class Database
 	public static function access($key)
 	{
 
+		$SERVER_NAME = "localhost";
+		$USER_NAME = "root";
+		$PASSWORD = "";
+
 		if ($key == "prueba") {
 
-			$db = new mysqli('localhost', 'root', '', 'dbsystem');
+			$db = new mysqli($SERVER_NAME,$USER_NAME, $PASSWORD, 'dbsystem');
 			$db->query("SET NAMES 'utf8'");
 
 			if ($db->connect_errno) {
@@ -29,11 +39,12 @@ class Database
 				exit();
 			}
 
+			$_SESSION['DATABASE_NAME'] = "dbsystem";
 			return $db;
 
 		} else if ($key == "prueba2") {
 
-			$db = new mysqli('localhost', 'root', '', 'dbsystem2');
+			$db = new mysqli($SERVER_NAME,$USER_NAME, $PASSWORD, 'dbsystem2');
 			$db->query("SET NAMES 'utf8'");
 
 			if ($db->connect_errno) {
@@ -41,10 +52,32 @@ class Database
 				exit();
 			}
 
+			$_SESSION['DATABASE_NAME'] = "dbsystem2";
+			return $db;
+
+		} else if ($key == "prueba3") {
+
+			$db = new mysqli($SERVER_NAME,$USER_NAME, $PASSWORD, 'dbsystem3');
+			$db->query("SET NAMES 'utf8'");
+
+			if ($db->connect_errno) {
+				printf("Connect failed: %s\n", $db->connect_error);
+				exit();
+			}
+
+			$_SESSION['DATABASE_NAME'] = "dbsystem2";
 			return $db;
 
 		}
 	}
+
+
+
+
+
+
+
+
 }
 
 
